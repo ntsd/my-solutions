0acd461ee6ac97a69c468e4acd60bd8e8eaa4d6b
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-02-21 05:16:46 +0700

    Create ProjectEuler1-Multipleof3and5.py

---
+++ Hackerrank/contest/ProjectEuler+/ProjectEuler1-Multipleof3and5.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+def sumOfNaturalNumber(num):
+    sum=0
+    three=num//3
+    five=num//5
+    fifteen=num//15
+    sum+=3*(three*(three+1)//2)
+    sum+=5*(five*(five+1)//2)
+    sum-=15*(fifteen*(fifteen+1)//2)
+    return sum
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    n = int(raw_input().strip())
+    print(sumOfNaturalNumber(n-1))
