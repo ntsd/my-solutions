c4b26a98dc0668c493071a61667a831029d73763
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-02-21 04:20:53 +0700

    add Even Fibonacci numbers

---
+++ EvenFibonaccinumbers.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import sys
+
+def evenFibonacci(n):
+    a=1
+    b=2
+    ans=0
+    mode=1
+    while 1:
+        if mode:
+            if a>n:
+                return ans
+            if a %2==0:
+                ans+=a
+            a=a+b
+            mode=0
+        else:   
+            if b>n:
+                return ans
+            if b %2==0:
+                ans+=b
+            b=a+b
+            mode=1
+    
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())
+    print(evenFibonacci(n))
