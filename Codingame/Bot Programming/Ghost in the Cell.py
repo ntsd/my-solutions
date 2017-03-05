fe23994977ddda4d0c2e99adc4cd49ba03de3e80
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-03-06 01:48:34 +0700

    Update Ghost in the Cell.py

--- Codingame/Bot Programming/Ghost in the Cell.py
+++ Codingame/Bot Programming/Ghost in the Cell.py
@@ -1,18 +1,32 @@
 import sys
 import math
 
-def sortDiraction(links, directForm):
+moved = 0
+
+def printl(string):
+    if moved == 0:print(string)
+
+def closetDiraction(links, directTo, factorys, minTroops):
     closetId = None
-    closetDistance = 0
+    closetDistance = 999999
+    if minTroops <= 1:
+        minTroops = 1
     for i in links:
-        if i.diractFrom == directForm or closetId== None:
-            closetId = i.diractTo
-            closetDistance = i.distance
+        if i.directFrom == directTo:           
+            myfactory = findFactoryFromId(factorys, i.directTo)
+            if myfactory!= None and ((myfactory.troops-5 > minTroops and i.distance < closetDistance)):
+                closetId = myfactory.id
+                closetDistance = i.distance
+        if i.directTo == directTo:
+            myfactory = findFactoryFromId(factorys, i.directFrom)
+            if myfactory!= None and ((myfactory.troops-5 > minTroops and i.distance < closetDistance)):
+                closetId = myfactory.id
+                closetDistance = i.distance
     return [closetId, closetDistance]
 
-def getDistanceFromLinks(links, directForm, directTo):
+def getDistanceFromLinks(links, directFrom, directTo):
     for i in links:
-        if i.directForm == directForm and i.directTo == directTo:
+        if i.directFrom == directFrom and i.directTo == directTo:
             return distance
 
 def findFactoryFromId(factorys, id):
@@ -21,8 +35,8 @@ def findFactoryFromId(factorys, id):
             return i
  
 class Link:
-    def __init__(self, diractFrom, directTo, distance):
-        self.diractFrom = diractFrom
+    def __init__(self, directFrom, directTo, distance):
+        self.directFrom = directFrom
         self.directTo = directTo
         self.distance = distance
         
@@ -38,15 +52,22 @@ class Factory:
         self.troopsInto = 0
     
     def __eq__(self, other):
-        return self.id == other.id
+        try:
+            return self.id == other.id 
+        except:
+            return self.id == other
+    
+    def __lt__(self, other):
+         return self.troops < other.troops
+        
 
 class Troop:
-    def __init__(self, id, player, diractFrom, directTo, number, remaining ):
+    def __init__(self, id, player, directFrom, directTo, troops, remaining ):
         self.id = id
         self.player = player
-        self.diractFrom = diractionFrom
-        self.directTo = directionTo
-        self.number = number
+        self.directFrom = directFrom
+        self.directTo = directTo
+        self.troops = troops
         self.remaining  = remaining
       
 factory_count = int(input())  # the number of factories
@@ -59,12 +80,13 @@ for i in range(link_count):
     links.append(Link(factory_2, factory_1, distance))
     
 while True:
+    moved=0
     entity_count = int(input())  # the number of entities (e.g. factories and troops)
-    myfactorys = []
+    myFactorys = []
     opFactorys = []
     naFactorys = []
-    myTroops = []
-    opTroops = []
+    myTroops = {}
+    opTroops = {}
     for i in range(entity_count):
         entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 = input().split()
         entity_id = int(entity_id)
@@ -73,31 +95,73 @@ while True:
         arg_3 = int(arg_3)
         arg_4 = int(arg_4)
         arg_5 = int(arg_5)
+        # print(entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 , file=sys.stderr)
         if entity_type == "FACTORY":
-            if arg_1 == "1":
-                myfactorys.append(Factory(entity_id, arg_1, arg_2, arg_3))
-            elif arg_1 == "-1":
+            if arg_1 == 1:
+                myFactorys.append(Factory(entity_id, arg_1, arg_2, arg_3))
+            elif arg_1 == -1:
                 opFactorys.append(Factory(entity_id, arg_1, arg_2, arg_3))
-            elif arg_1 == "0":
+            elif arg_1 == 0:
                 naFactorys.append(Factory(entity_id, arg_1, arg_2, arg_3))
             
         elif entity_type == "TROOP":
-            if arg_1 == "1":
-                myTroops.append(Troop(entity_id, arg_1, arg_2, arg_3, arg_4, arg_5))
-            elif arg_1 == "-1":
-                opTroops.append(Troop(entity_id, arg_1, arg_2, arg_3, arg_4, arg_5))
+            if arg_1 == 1:
+                try:
+                    myTroops[arg_3].append(Troop(entity_id, arg_1, arg_2, arg_3, arg_4, arg_5))
+                except:
+                    myTroops[arg_3] = []
+                
+            elif arg_1 == -1:
+                try:
+                    opTroops[arg_3].append(Troop(entity_id, arg_1, arg_2, arg_3, arg_4, arg_5))
+                except:
+                    opTroops[arg_3] = []
+                
     
     # To debug: print("Debug messages...", file=sys.stderr)
     for i in opTroops:
-        myClosetId, myClosetDistance = sortDiraction(links, i.directTo)
-        myClosetFactory = findFactoryFromId(myfactorys, myClosetId)
-        distanceFromTroopToTarget = getDistanceFromLinks(links, i.directFrom, i.directTo)
-        if i.directTo in naFactorys:
-            if i.remaining+distanceFromTroopToTarget < myClosetDistance*2:
-                print("MOVE "+myClosetId+" "+i.directTo+" "+(i.number+1))
-        elif i.directTo in myfactorys:
-            if i.remaining+distanceFromTroopToTarget < myClosetDistance*2:
-                print("MOVE "+myClosetId+" "+i.directTo+" "+(i.number+1))
+        # distanceFromTroopToTarget = getDistanceFromLinks(links, i.directFrom, i.directTo)
+        troops = sum(j.troops for j in opTroops[i])
+        if i in naFactorys:
+            # print("i.directTo in naFactorys", i.directTo, file=sys.stderr)
+            factoryTo = findFactoryFromId(naFactorys, i)
+            closetId, closetDistance = closetDiraction(links, i, myFactorys, troops)
+            closetFactory = findFactoryFromId(myFactorys, closetId) or findFactoryFromId(naFactorys,closetId)
+            if closetFactory!=None and troops-factoryTo.troops < closetFactory.troops and troops-factoryTo.troops > 0:
+                if moved==0 and closetId != None:print("MOVE",closetId,i,(troops-factoryTo.troops+4));moved = 1
+        elif i in myFactorys:
+            print("i.directTo in myFactorys", i, file=sys.stderr)
+            factoryTo = findFactoryFromId(myFactorys, i)
+            closetId, closetDistance = closetDiraction(links, i, myFactorys, troops)
+            print(closetId, i, file=sys.stderr)
+            closetFactory = findFactoryFromId(myFactorys, closetId) or findFactoryFromId(naFactorys, closetId)
+            if closetFactory!=None and troops-factoryTo.troops < closetFactory.troops and troops-factoryTo.troops>0:
+                if moved==0 and closetId != None:print("MOVE",closetId,i,(troops-factoryTo.troops));moved = 1
+                
+    
+    bestFromId = None
+    bestToId = None
+    bestTroops = None
+    bestProduction = None
+    bestDistance = None
     for i in naFactorys:
-        pass
+        # print("naFactorys" , file=sys.stderr)
+        if i.production > 0:
+            closetId, closetDistance = closetDiraction(links, i.id, myFactorys, i.troops)
+            closetFactory = findFactoryFromId(myFactorys, closetId)
+            if moved==0 and closetId!=None:print("MOVE "+str(closetId)+" "+str(i.id)+" "+str(i.troops+1));move = 1
+    for i in sorted(opFactorys):
+        if i.production > 2:
+            for j in myFactorys:
+                if j.troops > i.troops:
+                    if moved==0:print("MOVE",j.id,i.id,str(i.troops+4))
+    for i in sorted(opFactorys):
+        if i.production > 1:
+            for j in myFactorys:
+                if j.troops > i.troops:
+                    if moved==0:print("MOVE",j.id,i.id,str(i.troops+4))
+    for i in sorted(opFactorys):
+        for j in myFactorys:
+            if j.troops > i.troops:
+                if moved==0:print("MOVE",j.id,i.id,str(i.troops+4))
     print("WAIT")
