7ee50156bb89c1f99a15ac618ccf7cf6b379f4dd
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-07-08 05:30:03 +0700

    Updated

--- Codingame/Code Golf/Don't Panic.py
+++ Codingame/Code Golf/Don't Panic.py
@@ -1,7 +1,12 @@
-z=int
+code = """z=int
 b=input
-j=b().split()
+s=str.split
+j=s(b())
 e={}
 e[j[3]]=e['-1']=z(j[4])
-for i in range(z(j[7])):f,q=b().split();e[f]=z(q)
-while 1:f,p,d=b().split();print(["WAIT","BLOCK"][[e[f]<z(p),e[f]>z(p)][d[0]=="L"]])
+for _ in range(z(j[7])):f,q=s(b());e[f]=z(q)
+while 1:f,p,d=s(b());print("BWLAOICTK"[[e[f]<=z(p),e[f]>=z(p)][d[0]>"L"]::2])""".encode().decode('utf16')
+
+print(code)
+
+exec(bytes(code,'u16')[2:])
---
+++ Codingame/Code Golf/Power of Thor.py
@@ -0,0 +1,6 @@
+code = """x,y,a,b=map(int,input().split())
+while 1:print(["","S"][y>b]+["",["W","E"][x>a]][x!=a]);b+=1""".encode().decode('utf16')
+
+print(code)
+
+exec(bytes(code,'u16')[2:])
