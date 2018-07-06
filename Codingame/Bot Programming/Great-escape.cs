39f685f951182979a49af3d09205f7584da4a62c
Jiravat Boonkumnerd <jo06942@gmail.com>
2018-07-06 22:07:28 +0700

    update great escape

--- Codingame/Bot Programming/Great-escape.cs
+++ Codingame/Bot Programming/Great-escape.cs
@@ -16,20 +16,37 @@ class Path
         this.next = next;
         this.path = path;
     }
+    
+    public override string ToString()
+    {
+        if(this.path.Count > 0)return String.Format("(cost:{0},this:{1},next:{2})",this.cost, this.path[this.path.Count-1], this.next);
+        return String.Format("(cost:{0},next:{1})",this.cost, this.next);
+    }
 }
 
-class Heap
+class HeapQueue
 {
-    public int count = 0;
-    public SortedList<int, Path> paths = new SortedList<int, Path>();
-
+    public int count;
+    public int index;
+    public SortedList<int, Path> paths;
+    
+    public HeapQueue()
+    {
+        count = 0;
+        index = 0;
+        paths = new SortedList<int, Path>();
+    }
+    
     public void Add(Path path){
-        this.paths.Add(path.cost, path);
+        // Console.Error.WriteLine(path.ToString());
+        this.paths.Add(path.cost*1000000+this.index, path);
         this.count++;
+        this.index++;
     }
     
     public Path Pop(){
-        Path path = paths[0];
+        Path path = paths[paths.Keys[0]]; // get frist element
+        Console.Error.WriteLine(path.ToString());
         paths.RemoveAt(0);
         count--;
         return path;
@@ -59,6 +76,11 @@ class Point
     public override int GetHashCode() 
     {
         return  this.x ^ this.y;
+    }
+    
+    public override string ToString()
+    {
+        return String.Format("({0},{1})",this.x, this.y);
     }
 }
 
@@ -100,12 +122,13 @@ class Player
     public int x { get; set; }
     public int y { get; set; }
     public int walls_left { get; set; }
-    List<Point> targets { get; set; }
+    public List<Point> targets { get; set; }
     
     public Player(int id, List<Point> targets)
     {
-       this.id = id;
-       this.targets = targets;
+        this.id = id;
+        this.targets = targets;
+        // Console.Error.WriteLine(id.ToString() +' '+ targets[0].ToString());
     }
     
     public void update(string[] inputs)
@@ -115,31 +138,47 @@ class Player
         this.walls_left = int.Parse(inputs[2]);
     }
     
-    public Path shortest_path_closet(Dictionary<Point, List<Point>> graph, List<Point> ends){
-        Heap heapq = new Heap();
-        heapq.Add(new Path(0, new Point(this.x, this.y), new List<Point>()));
+    public Path shortest_path_closet(Dictionary<Point, List<Point>> graph){
+        Queue<Path> queue = new Queue<Path>();
+        queue.Enqueue(new Path(0, new Point(this.x, this.y), new List<Point>()));
         HashSet<Point> seen = new HashSet<Point>();
-        Path path;
-        while(heapq.count > 0){
-            path = heapq.Pop();
-            if(!seen.Contains(path.next)){
-                path.path.Add(path.next);
-                seen.Add(path.next);
-                if(ends.Contains(path.next))return path;
-                foreach(Point next in graph[path.next]){
-                    heapq.Add(new Path(path.cost+1, next, path.path));
+        while(queue.Count > 0){
+            Path path_ = queue.Dequeue();
+            if(!seen.Contains(path_.next)){
+                List<Point> new_path = path_.path;
+                new_path.Add(path_.next);
+                seen.Add(path_.next);
+                // if(this.targets.Contains(path_.next))return path_;
+                foreach(Point t in this.targets){
+                    if(t.x == path_.next.x && t.y==path_.next.y)return path_;
+                }
+                foreach(Point next in graph[path_.next]){
+                    queue.Enqueue(new Path(path_.cost+1, next, new_path));
                 }
             }
         }
         return null;// return null if not see path
+    }
+    
+    public string getDirection(Point next_point){
+        if(next_point.x < this.x)return "LEFT";
+        if(next_point.x > this.x)return "RIGHT";
+        if(next_point.y < this.y)return "UP";
+        if(next_point.y > this.y)return "DOWN";
+        return "NONE";
     }
 }
 
 
 class Program
 {
-    static string action(){
-        return "RIGHT";
+    static string action(Player myPlayer, List<Player> opPlayer, Dictionary<Point, List<Point>> graph){// todo
+        
+        
+        Path myShortestPath = myPlayer.shortest_path_closet(graph);
+        
+        Console.Error.WriteLine(myShortestPath.path[1].ToString());
+        return myPlayer.getDirection(myShortestPath.path[1]);
     }
     
     static void Main(string[] args)
@@ -193,14 +232,15 @@ class Program
                 int wall_y = int.Parse(inputs[1]);
                 string orientation = inputs[2]; // ('H' or 'V')
                 Wall wall = new Wall(wall_x, wall_y, orientation);
-                if(walls.Exists(w_ => w_.isCross(wall))){
-                    if(orientation.Equals('H')){
+                if(!walls.Exists(w_ => w_.isCross(wall))){
+                    if(orientation.Equals("H")){
                         graph[new Point(wall_x, wall_y)].Remove(new Point(wall_x, wall_y-1));
                         graph[new Point(wall_x, wall_y-1)].Remove(new Point(wall_x, wall_y));
                         graph[new Point(wall_x+1, wall_y)].Remove(new Point(wall_x+1, wall_y-1));
                         graph[new Point(wall_x+1, wall_y-1)].Remove(new Point(wall_x+1, wall_y));
                     }
-                    else if(orientation.Equals('V')){
+                    else if(orientation.Equals("V")){
+                        // Console.Error.WriteLine("wall add");
                         graph[new Point(wall_x, wall_y)].Remove(new Point(wall_x-1, wall_y));
                         graph[new Point(wall_x-1, wall_y)].Remove(new Point(wall_x, wall_y));
                         graph[new Point(wall_x, wall_y+1)].Remove(new Point(wall_x-1, wall_y+1));
@@ -209,8 +249,12 @@ class Program
                     walls.Add(wall);
                 }
             }
-
-            Console.WriteLine(action());
+            
+            Player myPlayer = players[myId];
+            List<Player> opPlayer = players.Where( p => p.id != myId && p.x != -1).ToList();
+            
+            string output = action(myPlayer, opPlayer, graph);
+            Console.WriteLine(output);
         }
     }
 }
