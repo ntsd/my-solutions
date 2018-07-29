7db54fd7d05ac71749dd6ddb6cd79921cab4ec10
Jiravat Boonkumnerd <jo06942@gmail.com>
2018-07-29 13:56:39 +0700

    Add attack rules

--- Codingame/Bot-Programming/legends-of-code-and-magic.py
+++ Codingame/Bot-Programming/legends-of-code-and-magic.py
@@ -1,11 +1,16 @@
 import sys
 import math
+import copy
 
 log=sys.stderr
 
+class AttackRule:
+    def __init__(self, sortMy, myFilter, sortOp, opFilter, rule):
+        self.sortMy, self.myFilter, self.sortOp, self.opFilter, self.rule = sortMy, myFilter, sortOp, opFilter, rule
+
 class Player:
     def __init__(self):
-        self.mana_curve = [0, 7, 6, 5, 4, 3, 0, 0, 0, 0, 0, 0, 0] # 1-6
+        self.mana_curve = [0, 7, 6, 5, 4, 3, 0, 0, 0, 0, 0, 0, 0]
     def setEnemy(self, player):
         self.op = player
     def update(self, player_health, player_mana, player_deck, player_rune):
@@ -21,13 +26,14 @@ class Player:
         max_value = 0
         card_no=0
         print(self.mana_curve, file=log)
-        for i, c in enumerate(draft_list):
-            value = c.attack * c.defense
-            if self.mana_curve[c.cost] > 0:
-                value += 100
-            if value > max_value:
-                max_value = value
-                card = i
+        for i, card in enumerate(draft_list):
+            if type(card) is Creature: #todo add item pick
+                value = card.attack * card.defense
+                if self.mana_curve[card.cost] > 0:
+                    value += 100
+                if value > max_value:
+                    max_value = value
+                    card_no = i
         self.mana_curve[draft_list[card_no].cost]-=1
         print('PICK', card_no)
      
@@ -35,104 +41,80 @@ class Player:
         mana=self.player_mana
         action_list = []
         boards_count = len(self.boards)
-        for c in sorted(self.hands, key=lambda x: x.cost, reverse=True):
+        creature_on_hand = filter(lambda x: type(x) is Creature ,self.hands)
+        for c in sorted(creature_on_hand, key=lambda x: x.cost, reverse=True):
             if boards_count < 6 or 1: # to do need to check summon again after trade
                 if c.cost<=mana:
                     mana-=c.cost
                     action_list.append("SUMMON {}".format(c.instance_id))
-                    if 'C' in c.abilities:
+                    if c.charge:
                         self.boards.append(c)
                     boards_count += 1
         return action_list
     
-    def attack(self):
+    
+    def creatureAttack(self):
         action_list = []
-        # check trade taunt value
-        for my_c in sorted(self.boards, key=lambda x: x.defense, reverse=True):
-            if my_c.action==0:
-                continue
-            for op_c in sorted(self.op.boards, key=lambda x: x.defense, reverse=True):
-                if 'G' in op_c.abilities:
-                    if my_c.attack >= op_c.defense and my_c.defense > op_c.attack:
-                        action_list.append("ATTACK {} {}".format(my_c.instance_id, op_c.instance_id))
-                        my_c.action = 0
-                        if op_c.shield:
-                            op_c.shield=0
-                        else:
-                            self.op.boards.remove(op_c)
-                        break
         
-        # check attack taunt
-        for my_c in sorted(self.boards, key=lambda x: x.attack):
-            if my_c.action==0:
-                continue
-            for op_c in sorted(self.op.boards, key=lambda x: x.attack, reverse=True):
-                if 'G' in op_c.abilities:
-                    action_list.append("ATTACK {} {}".format(my_c.instance_id, op_c.instance_id))
-                    my_c.action = 0
-                    op_c.defense -= my_c.attack
-                    if op_c.shield:
-                        op_c.shield=0
-                    else:
-                        if op_c.defense <= 0:
-                            self.op.boards.remove(op_c)
-                    break
+        attack_rules=[
+            # trade taunt value
+            AttackRule(lambda my: -my.defense, lambda my: my.action,
+            lambda op: -op.attack, lambda op: op.guard==1,
+            lambda my, op: my.live and op.live==0),
+            # attack taunt and survive
+            AttackRule(lambda my: -my.attack, lambda my: my.action,
+            lambda op: -op.attack, lambda op: op.guard==1,
+            lambda my, op:  my.live),
+            # attack taunt
+            AttackRule(lambda my: -my.attack, lambda my: my.action,
+            lambda op: -op.attack, lambda op: op.guard==1,
+            lambda my, op: True),
+             # trade lethal 
+            AttackRule(lambda my: my.defense, lambda my: my.action,
+            lambda op: -op.attack, lambda op: op.lethal==1,
+            lambda my, op: op.live==0),
+            # trade value
+            AttackRule(lambda my: my.attack, lambda my: my.action,
+            lambda op: -op.attack, lambda op: True,
+            lambda my, op: my.live and op.live==0),
+            # trade equal
+            AttackRule(lambda my: my.attack, lambda my: my.action,
+            lambda op: -op.attack, lambda op: True,
+            lambda my, op: op.live==0), # and self.player_health < self.op.player_health
+        ]
         
-        # check trade lethal 
-        for my_c in sorted(self.boards, key=lambda x: x.defense, reverse=True):
-            if my_c.action==0:
-                continue
-            for op_c in sorted(self.op.boards, key=lambda x: x.defense, reverse=True):
-                if 'L' in op_c.abilities:
-                    if my_c.attack >= op_c.defense and my_c.defense > op_c.attack:
-                        action_list.append("ATTACK {} {}".format(my_c.instance_id, op_c.instance_id))
+        for attack_rule in attack_rules:
+            for my_c in sorted(filter(attack_rule.myFilter, self.boards), key=attack_rule.sortMy):
+                for op_c in sorted(filter(attack_rule.opFilter, self.op.boards), key=attack_rule.sortOp):
+                    my_c_temp = copy.deepcopy(my_c)
+                    op_c_temp = copy.deepcopy(op_c)
+                    my_c_temp.attackTarget(op_c_temp)
+                    if attack_rule.rule(my_c_temp, op_c_temp):
+                        my_c = my_c_temp
+                        op_c = op_c_temp
+                        action_list.append("ATTACK {} {} trade".format(my_c.instance_id, op_c.instance_id))
                         my_c.action = 0
-                        if op_c.shield:
-                            op_c.shield=0
-                        else:
-                            self.op.boards.remove(op_c)
-                        break
-        
-        # check trade value
-        for my_c in sorted(self.boards, key=lambda x: x.defense, reverse=True):
-            if my_c.action==0:
-                continue
-            for op_c in sorted(self.op.boards, key=lambda x: x.defense, reverse=True):
-                if my_c.attack >= op_c.defense and my_c.defense > op_c.attack:
-                    action_list.append("ATTACK {} {}".format(my_c.instance_id, op_c.instance_id))
-                    my_c.action = 0
-                    if op_c.shield:
-                        op_c.shield=0
-                    else:
-                        self.op.boards.remove(op_c)
-                    break
-            
-        # check trade equal
-        if self.player_health < self.op.player_health:
-            for my_c in sorted(self.boards, key=lambda x: x.attack):
-                if my_c.action==0:
-                    continue
-                for op_c in sorted(self.op.boards, key=lambda x: x.attack, reverse=True):
-                    if my_c.attack >= op_c.defense:
-                        action_list.append("ATTACK {} {}".format(my_c.instance_id, op_c.instance_id))
-                        my_c.action = 0
-                        if op_c.shield:
-                            op_c.shield=0
-                        else:
+                        if not my_c.live:
+                            self.boards.remove(my_c)
+                        if not op_c.live:
                             self.op.boards.remove(op_c)
                         break
         # go face
         for my_c in self.boards:
             if my_c.action:
-                action_list.append("ATTACK {} {}".format(my_c.instance_id, -1))
+                action_list.append("ATTACK {} {} face".format(my_c.instance_id, -1))
                 my_c.action = 0
 
         return action_list
-
+    
+    def useItem(self):
+        return []
+    
     def play(self):
         action_list = []
         action_list += self.summon()
-        action_list += self.attack()
+        action_list += self.useItem()
+        action_list += self.creatureAttack()
         print(';'.join(action_list))
 
 class Card:
@@ -140,13 +122,39 @@ class Card:
         self.card_number, self.instance_id, self.location, self.card_type, self.cost, self.attack, self.defense, self.abilities, self.my_health_change, self.opponent_health_change, self.card_draw = card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw
         self.action = 1
         self.shield = 1 if 'W' in abilities else 0
+        self.lethal = 1 if 'L' in abilities else 0
+        self.guard = 1 if 'G' in abilities else 0
+        self.charge = 1 if 'C' in abilities else 0
+        self.drain = 1 if 'D' in abilities else 0
+        self.breakthrough = 1 if 'B'in abilities else 0
+        self.live = 1
     def __hash__(self):
          return hash(self.instance_id)
     def __eq__(self, other):
         return (
             self.__class__ == other.__class__ and
             self.instance_id == other.instance_id
-        )    
+        )
+
+class Item(Card):
+    def use(self, target):
+        pass
+
+class Creature(Card):      
+    def attackTarget(self, target): # attack op creature
+        self.action = 0
+        if self.shield: # damage to self
+            self.shield = 0
+        else:
+            self.defense -= target.attack
+            if self.defense <=0 or (target.lethal and type(target) is Creature):
+                self.live = 0
+        if target.shield: # damage to target
+            target.shield = 0
+        else:
+            target.defense -= self.attack
+            if target.defense <=0 or (self.lethal and type(target) is Creature):
+                target.live = 0
     
 my_player = Player()
 op_player = Player()
@@ -162,18 +170,11 @@ while True:
     card_count = int(input())
     draft_list = []
     for i in range(card_count):
-        card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw = input().split()
-        card_number = int(card_number)
-        instance_id = int(instance_id)
-        location = int(location)
-        card_type = int(card_type)
-        cost = int(cost)
-        attack = int(attack)
-        defense = int(defense)
-        my_health_change = int(my_health_change)
-        opponent_health_change = int(opponent_health_change)
-        card_draw = int(card_draw)
-        card = Card(card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+        card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw = map(lambda x: int(x) if x[-1].isdigit() else x,input().split())
+        if card_type!=0:
+            card = Item(card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+        else:
+            card = Creature(card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
         if my_player.player_mana==0:
             draft_list.append(card)
         elif location==0:
