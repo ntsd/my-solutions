3f803c7e39baa0fd863848f3f4350079c800e09c
ntsd <jo06942@gmail.com>
2019-10-19 00:00:02 +0700

    Add 12-days-of-christmas

---
+++ code-golf.io/12-days-of-christmas/12-days-of-christmas.py
@@ -0,0 +1,95 @@
+a='Two Turtle Doves, and'
+b='Three French Hens,'
+c='Four Calling Birds,'
+d='Five Gold Rings,'
+e='Six Geese-a-Laying,'
+f='Seven Swans-a-Swimming,'
+g='Eight Maids-a-Milking,'
+h='Nine Ladies Dancing,'
+i='Ten Lords-a-Leaping,'
+j='Eleven Pipers Piping,'
+k=lambda x:f'On the {x} day of Christmas'
+x="My true love sent to me"
+y="A Partridge in a Pear Tree."
+z=f"""{b}
+{a}
+{y}
+
+"""
+print(f"""{k('First')}
+{x}
+{y}
+
+{k('Second')}
+{x}
+{a}
+{y}
+
+{k('Third')}
+{x}
+{z}{k('Fourth')}
+{x}
+{c}
+{z}{k('Fifth')}
+{x}
+{d}
+{c}
+{z}{k('Sixth')}
+{x}
+{e}
+{d}
+{c}
+{z}{k('Seventh')}
+{x}
+{f}
+{e}
+{d}
+{c}
+{z}{k('Eighth')}
+{x}
+{g}
+{f}
+{e}
+{d}
+{c}
+{z}{k('Ninth')}
+{x}
+{h}
+{g}
+{f}
+{e}
+{d}
+{c}
+{z}{k('Tenth')}
+{x}
+{i}
+{h}
+{g}
+{f}
+{e}
+{d}
+{c}
+{z}{k('Eleventh')}
+{x}
+{j}
+{i}
+{h}
+{g}
+{f}
+{e}
+{d}
+{c}
+{z}{k('Twelfth')}
+{x}
+Twelve Drummers Drumming,
+{j}
+{i}
+{h}
+{g}
+{f}
+{e}
+{d}
+{c}
+{b}
+{a}
+{y}""")
