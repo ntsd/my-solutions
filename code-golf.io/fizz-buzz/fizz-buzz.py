9b89296fd353d7579f643a686d47c31149e40612
ntsd <jo06942@gmail.com>
2019-10-19 00:56:56 +0700

    Add fizz-buzz

---
+++ code-golf.io/fizz-buzz/fizz-buzz.py
@@ -0,0 +1,10 @@
+# for i in range(1,101):print('Fizz'*(i%3==0)+'Buzz'*(i%5==0)or i)
+
+i=1
+while i<101:print('Fizz'*(i%3<1)+'Buzz'*(i%5<1)or i);i+=1
+
+# exec("print('Fizz'*(i%3<1)+'Buzz'*(i%5<1)or i);i+=1;"*100)
+
+
+
+
