2754f99d2ce6c3d1168f8921663d8b4fdf723f4e
ntsd <jo06942@gmail.com>
2019-10-25 20:24:35 +0700

    Updated divisors

--- code-golf.io/divisors/divisors.py
+++ code-golf.io/divisors/divisors.py
@@ -3,5 +3,8 @@
 # i=1
 # exec('print(*[j for j in range(1,i+1)if i%j<1]);i+=1;'*100)
 
-i=2
+i=1
 while i<101:print(*[j for j in range(1,i+1)if i%j<1]);i+=1
+
+i=2
+while i<102:print(*[j for j in range(1,i)if~-i%j<1]);i+=1
