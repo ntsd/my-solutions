ad12a911f0c4c90b6111447b973ffaa093aad15c
ntsd <jo06942@gmail.com>
2020-03-13 10:46:23 +0700

    Add instragram unfollow

---
+++ instagram-unfollow-loop.js
@@ -0,0 +1,7 @@
+// open profile page
+// open following window
+setInterval(async () => {
+  document.getElementsByClassName("sqdOP  L3NKy    _8A5w5    ").item(1).click()
+  await new Promise(r => setTimeout(r, 5000));
+  document.getElementsByClassName("aOOlW -Cab_   ").item(0).click()
+}, 30000)
