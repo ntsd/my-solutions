b67ea2cdb6fc0a4c84505a1f7a488ea8ab2e7c21
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-04-11 05:30:03 +0700

    feat: update dots and boxes to alpha beta pruning

--- Codingame/Bot-Programming/dots-and-boxes.cpp
+++ Codingame/Bot-Programming/dots-and-boxes.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <limits>
 #include <set>
@@ -6,6 +7,7 @@
 #include <vector>
 
 using namespace std;
+using namespace std::chrono;
 
 int boardSize;
 char playerId;
@@ -36,95 +38,152 @@ bool isSidePlayed(const string &boxName, char side) {
 
 vector<pair<string, char>> getValidMoves() {
   vector<pair<string, char>> validMoves;
-  for (const Box &box : boxes) {
-    for (char side : box.sides) {
-      if (!isSidePlayed(box.name, side)) {
+
+  for (const auto &box : boxes) {
+    for (const auto &side : box.sides) {
+      if (playedSides.find({box.name, side}) == playedSides.end()) {
         validMoves.push_back({box.name, side});
       }
     }
   }
+
   return validMoves;
 }
 
-int minimax(int depth, bool isMax, int playerScore, int opponentScore) {
-  if (depth == 0 || numBoxes == playerScore + opponentScore) {
+int alphabeta(int depth, bool isMax, int alpha, int beta, int playerScore,
+              int opponentScore) {
+  if (depth == 0) {
+    return playerScore - opponentScore;
+  }
+
+  vector<pair<string, char>> validMoves = getValidMoves();
+  if (validMoves.empty()) {
     return playerScore - opponentScore;
   }
 
   if (isMax) {
-    int best = numeric_limits<int>::min();
-    for (Box &box : boxes) {
-      if (box.sides.size() == 1) {
+    int bestVal = numeric_limits<int>::min();
+    for (const auto &[boxName, side] : validMoves) {
+      auto it = find_if(boxes.begin(), boxes.end(), [boxName](const Box &box) {
+        return box.name == boxName;
+      });
+      if (it == boxes.end())
+        continue;
+      Box &box = *it;
+
+      string prevSides = box.sides;
+      box.sides.erase(remove(box.sides.begin(), box.sides.end(), side),
+                      box.sides.end());
+      int prevScore = playerScore;
+      if (box.sides.empty())
         playerScore++;
-        box.sides = "";
-        best =
-            max(best, minimax(depth - 1, !isMax, playerScore, opponentScore));
-        playerScore--;
-        box.sides = "L";
+
+      int moveVal =
+          alphabeta(depth - 1, !isMax, alpha, beta, playerScore, opponentScore);
+
+      box.sides = prevSides;
+      playerScore = prevScore;
+
+      bestVal = max(bestVal, moveVal);
+      alpha = max(alpha, bestVal);
+
+      if (beta <= alpha) {
+        break;
       }
     }
-    return best;
+    return bestVal;
   } else {
-    int best = numeric_limits<int>::max();
-    for (Box &box : boxes) {
-      if (box.sides.size() == 1) {
+    int bestVal = numeric_limits<int>::max();
+    for (const auto &[boxName, side] : validMoves) {
+      auto it = find_if(boxes.begin(), boxes.end(), [boxName](const Box &box) {
+        return box.name == boxName;
+      });
+      if (it == boxes.end())
+        continue;
+      Box &box = *it;
+
+      string prevSides = box.sides;
+      box.sides.erase(remove(box.sides.begin(), box.sides.end(), side),
+                      box.sides.end());
+      int prevScore = opponentScore;
+      if (box.sides.empty())
         opponentScore++;
-        box.sides = "";
-        best =
-            min(best, minimax(depth - 1, !isMax, playerScore, opponentScore));
-        opponentScore--;
-        box.sides = "L";
+
+      int moveVal =
+          alphabeta(depth - 1, !isMax, alpha, beta, playerScore, opponentScore);
+
+      box.sides = prevSides;
+      opponentScore = prevScore;
+
+      bestVal = min(bestVal, moveVal);
+      beta = min(beta, bestVal);
+
+      if (beta <= alpha) {
+        break;
       }
     }
-    return best;
+    return bestVal;
   }
 }
 
 string getNextMove() {
-  int bestVal = numeric_limits<int>::min();
-  string bestMove = "";
-  vector<pair<string, char>> validMoves = getValidMoves();
+    int bestVal = numeric_limits<int>::min();
+    string bestMove = "";
+    vector<pair<string, char>> validMoves = getValidMoves();
+    int alpha = numeric_limits<int>::min();
+    int beta = numeric_limits<int>::max();
+    
+    auto start = steady_clock::now();
 
-  for (const auto &[boxName, side] : validMoves) {
-    // Find the corresponding box
-    auto it = find_if(boxes.begin(), boxes.end(), [boxName](const Box &box) {
-      return box.name == boxName;
-    });
-    if (it == boxes.end())
-      continue;
-    Box &box = *it;
+    for (const auto &[boxName, side] : validMoves) {
+        // Find the box and try the move
+        auto it = find_if(boxes.begin(), boxes.end(), [boxName](const Box &box) {
+            return box.name == boxName;
+        });
+        if (it == boxes.end()) continue;
+        Box &box = *it;
 
-    // Try the move
-    string prevSides = box.sides;
-    box.sides.erase(remove(box.sides.begin(), box.sides.end(), side),
-                    box.sides.end());
-    int prevScore = playerScore;
-    if (box.sides.empty())
-      playerScore++;
+        string prevSides = box.sides;
+        box.sides.erase(remove(box.sides.begin(), box.sides.end(), side), box.sides.end());
+        int prevScore = playerScore;
+        if (box.sides.empty()) playerScore++;
 
-    // Perform Minimax and find the best move
-    int moveVal = minimax(1000, false, playerScore, opponentScore);
+        // Add the side to the playedSides set
+        playedSides.insert({boxName, side});
 
-    // Undo the move
-    box.sides = prevSides;
-    playerScore = prevScore;
+        int moveVal = alphabeta(2, false, alpha, beta, playerScore, opponentScore);
 
-    // Update the best move
-    if (moveVal > bestVal) {
-      bestVal = moveVal;
-      bestMove = boxName + " " + side;
+        // Undo the move and update the best move
+        box.sides = prevSides;
+        playerScore = prevScore;
+
+        // Remove the side from the playedSides set
+        playedSides.erase({boxName, side});
+
+        if (moveVal > bestVal) {
+            bestVal = moveVal;
+            bestMove = boxName + " " + side;
+        }
+
+        alpha = max(alpha, bestVal);
+
+        auto end = steady_clock::now();
+        auto elapsed = duration_cast<milliseconds>(end - start);
+
+        if (elapsed.count() >= 95) {
+            break;
+        }
     }
-  }
 
-  if (bestMove.empty()) {
-    // If no best move is found based on Minimax, play the first available valid
-    // side
-    const auto &[boxName, side] = validMoves[0];
-    bestMove = boxName + " " + side;
-  }
+    if (bestMove.empty()) {
+        // If no best move is found based on Alpha-Beta pruning, play the first available valid side
+        const auto &[boxName, side] = validMoves[0];
+        bestMove = boxName + " " + side;
+    }
 
-  playedSides.insert({bestMove.substr(0, 2), bestMove[3]});
-  return bestMove;
+    // Add the best move to the playedSides set
+    playedSides.insert({bestMove.substr(0, 2), bestMove[3]});
+    return bestMove;
 }
 
 int main() {
